1c3cbf61eb4253ff69cf94c7acff58fc382ddbbe
Ali Faruk Yücel <34458230+alifarukyucel@users.noreply.github.com>
2024-03-30 18:59:11 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,18 +2,27 @@ sudo apt update -y
 sudo apt upgrade -y
 
 sudo apt install -y zsh curl git zip unzip
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-# Install NerdFont Terminus
-curl -Lo "Terminus_NerdFont.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/Terminus.zip
 
 git clone https://github.com/alifarukyucel/dotfiles-wsl.git ~/.dotfiles
 
+GIT_CONFIG_TXT="$HOME/.dotfiles/git_config.txt"
+GIT_CONFIG="$HOME/.gitconfig"
+
+email=$(grep 'email' "$GIT_CONFIG_TXT" | awk -F " = " '{print $2}')
+name=$(grep 'name' "$GIT_CONFIG_TXT" | awk -F " = " '{print $2}')
+
+git config --file "$GIT_CONFIG" user.email "$email"
+git config --file "$GIT_CONFIG" user.name "$name"
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install NerdFont JetBrainsMono
+curl -Lo "JetBrainsMono.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
 mkdir -p ~/.fonts
+unzip JetBrainsMono.zip -d ~/.fonts
+fc-cache -fv
 
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.fonts ~/.fonts
-
-unzip Terminus_NerdFont.zip -d ~/.fonts
-fc-cache -fv
